4e99155698f47d9c161bf0868aac915f733a9f64
andre campos <andre17.workspace@gmail.com>
2024-10-16 20:48:37 -0300

    Fix

--- tests/index.test.ts
+++
@@ -1,7 +0,0 @@
-import { ext } from '../src/index';
-
-describe('numeroPorExtenso', () => {
-    test('deve retornar o número por extenso corretamente', () => {
-        expect(ext(0)).toBe('zero');
-    });
-});
